73b9d1ea2590270df76e220a18b29282244c9fe3
Denis Bilokon <greenjuiced@gmail.com>
2023-01-06 16:15:59 +0200

    create a row version of bot

---
+++ decorators.py
@@ -0,0 +1,22 @@
+from assistant import WrongTypePhone, WrongLenPhone
+
+def input_error(func):
+
+    def wrapper(*args):
+        try:
+            return func(*args)
+        except IndexError:
+            return "Give me name, old phone and new phone"
+        except KeyError:
+            return "Enter correct username"
+        except ValueError:
+            return "Enter username"
+        except TypeError:
+            return "Not enough params for command"
+        except WrongLenPhone:
+            return "Length of phone's number is wrong"
+        except WrongTypePhone:
+            return 'Incorrect phone number'
+
+
+    return wrapper
